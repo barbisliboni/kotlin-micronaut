57b90860bebdc1b244d8b948adaf297234ad8ac1
barbisliboni <niinaliboni@gmail.com>
2022-03-16 19:36:08 -0300

    adding @CincuitBreaker annotation

--- store-service/src/main/kotlin/br/com/course/http/VehicleHttp.kt
+++ store-service/src/main/kotlin/br/com/course/http/VehicleHttp.kt
@@ -4,8 +4,10 @@ import br.com.course.dto.output.Vehicle
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.retry.annotation.CircuitBreaker
 
 @Client(id = "http://localhost:8080") //where the vehicle microservice is running
+@CircuitBreaker
 interface VehicleHttp {
     @Get("/vehicles/{id}")
     fun findById(@PathVariable id: Long): Vehicle
