822c1ee7e13e2cc341af57bd2fa2a3311c01edba
barbisliboni <niinaliboni@gmail.com>
2022-03-17 18:25:52 -0300

    reducing redundant information

--- store-service/src/main/kotlin/br/com/course/http/fallback/VehicleHttpFallback.kt
+++ store-service/src/main/kotlin/br/com/course/http/fallback/VehicleHttpFallback.kt
@@ -1,5 +1,6 @@
 package br.com.course.http.fallback
 
+import br.com.course.config.Connection
 import br.com.course.dto.output.Vehicle
 import br.com.course.http.VehicleHttp
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -12,8 +13,7 @@ class VehicleHttpFallback(
     private val objectMapper: ObjectMapper
 ): VehicleHttp {
     override fun findById(id: Long): Vehicle {
-        val jedisPool = JedisPool(JedisPoolConfig(), "127.0.0.1", 6379 ) //JedisPool provides a pool of connections to Redis to reuse on demand
-        val jedis = jedisPool.resource
+        var jedis = Connection.getConnection()
         val vehicleJSON = jedis.get(id.toString())
         val vehicle = objectMapper.readValue(vehicleJSON, Vehicle::class.java)
 
