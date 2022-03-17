be49e35fc07fbf920961712fd6422e1f87688670
barbisliboni <niinaliboni@gmail.com>
2022-03-16 22:05:03 -0300

    listening to kafka

--- store-service/src/main/kotlin/br/com/course/service/SaleService.kt
+++ store-service/src/main/kotlin/br/com/course/service/SaleService.kt
@@ -4,14 +4,20 @@ import br.com.course.dto.input.SaleInput
 import br.com.course.dto.output.Installment
 import br.com.course.dto.output.Sale
 import br.com.course.http.VehicleHttp
+import br.com.course.producer.SaleProducer
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.inject.Singleton
 import java.time.LocalDate
+import java.util.*
+import kotlin.collections.ArrayList
 
 @Singleton
 class SaleService(
-    private val vehicleService: VehicleService
+    private val vehicleService: VehicleService,
+    private val saleProducer: SaleProducer,
+    private val objectMapper: ObjectMapper
 ) {
-    fun makeSale(saleInput: SaleInput){
+    fun makeSale(saleInput: SaleInput): Sale{
         val vehicle = vehicleService.getVehicle(saleInput.vehicle.toLong())
         var installments: List<Installment> = ArrayList<Installment>()
         var installmentPrice = saleInput.price.divide(saleInput.installments.toBigDecimal())
@@ -25,5 +31,12 @@ class SaleService(
 
         var sale = Sale(saleInput.client, vehicle, saleInput.price, installments)
         println(sale)
+        saleConfirm(sale)
+        return sale;
+    }
+
+    fun saleConfirm(sale: Sale){
+        var saleJSON = objectMapper.writeValueAsString(sale)
+        saleProducer.publishSale(UUID.randomUUID().toString(), saleJSON)
     }
 }
